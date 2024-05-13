e7aa74e39fde86758c4d10a85168a73d710a192a
Yurii Antonchyk <xsat.php@gmail.com>
2024-05-13 11:56:40 +0300

    Refactor main js

--- js/main.js
+++ js/main.js
@@ -2,16 +2,22 @@ const storage = window.localStorage;
 const inputs = document.querySelectorAll("[contenteditable]");
 
 inputs.forEach((element) => {
-    element.addEventListener("input", (event) => {
-        const target_element_id = event.target.id;
+    restoreDefaultValueInElement(element);
 
-        storage.setItem(target_element_id, event.target.innerText)
-    });
+    element.addEventListener("input", elementEventOnInput);
+});
 
+function restoreDefaultValueInElement(element) {
     const element_id = element.id;
     const element_value = storage.getItem(element_id);
 
     if (element_value !== null) {
         element.innerText = element_value;
     }
-});
+}
+
+function elementEventOnInput(event) {
+    const target_element_id = event.target.id;
+
+    storage.setItem(target_element_id, event.target.innerText)
+}
